8367fbf005c3ebec2d82da0649e0c51fe9f7e14e
Luciano Vieira <contato@lucianovieirapro.com>
2022-12-15 13:28:55 -0300

    refactor(endpoint): create a new endpoint to save data and generate new id random for new data save.

--- src/main/java/com/example/webfluxapi/controller/PlaylistController.java
+++ src/main/java/com/example/webfluxapi/controller/PlaylistController.java
@@ -3,17 +3,19 @@ package com.example.webfluxapi.controller;
 import com.example.webfluxapi.document.Playlist;
 import com.example.webfluxapi.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-public class PlayllistController {
+public class PlaylistController {
     @Autowired
     PlaylistService service;
+
+    @GetMapping(value = "/")
+    public String homePage(){
+        return "API Online";
+    }
 
     @GetMapping(value ="/playlist")
     public Flux<Playlist> getPlaylist(){
@@ -24,7 +26,7 @@ public class PlayllistController {
         return service.findById(id);
     }
     @PostMapping(value = "/playlist")
-    public Mono<Playlist> save(Playlist playlist){
+    public Mono<Playlist> save(@RequestBody Playlist playlist){
         return service.save(playlist);
     }
 }
