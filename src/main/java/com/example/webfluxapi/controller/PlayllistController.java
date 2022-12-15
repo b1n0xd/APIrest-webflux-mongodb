a65943987b2adeafbe905e6907541c125217777d
Luciano Vieira <contato@lucianovieirapro.com>
2022-12-15 11:57:38 -0300

    refactor(endponit): create a new endpoint to save data.

--- src/main/java/com/example/webfluxapi/controller/PlayllistController.java
+++ src/main/java/com/example/webfluxapi/controller/PlayllistController.java
@@ -5,6 +5,7 @@ import com.example.webfluxapi.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -21,5 +22,9 @@ public class PlayllistController {
     @GetMapping(value = "/playlist/{id}")
     public Mono<Playlist> getPlaylistId(@PathVariable String id){
         return service.findById(id);
+    }
+    @PostMapping(value = "/playlist")
+    public Mono<Playlist> save(Playlist playlist){
+        return service.save(playlist);
     }
 }
